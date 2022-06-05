c645456136c5d4a6a239bc5bfea85a1b2cc5b805
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2022-06-05 15:07:58 +0900

    修正

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 from controllers.controller import app
 from workers.worker import ParentThread
-from flask_sqlalchemy import SQLAlchemy
-
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://******:******@localhost/jobdemo"
-db = SQLAlchemy(app)
-
-class Job(db.Model):
-    id = db.Column(db.String(64), primary_key=True)
-    status = db.Column(db.String(64))
 
 if __name__ == "__main__":
     t = ParentThread()
